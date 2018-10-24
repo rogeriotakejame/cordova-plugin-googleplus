c7fd90b245673af7e946242a686696ea4baed392
rogeriotakejame <rogerio.takejame@gmail.com>
2018-10-24 12:34:06 -0300

    Update GooglePlus.java

--- src/android/GooglePlus.java
+++ src/android/GooglePlus.java
@@ -244,7 +244,7 @@ public class GooglePlus extends CordovaPlugin implements GoogleApiClient.OnConne
                             if (task.isSuccessful()){
                                 savedCallbackContext.success("Logged user out");
                             } else {
-                                savedCallbackContext.error(task.getException());
+                                savedCallbackContext.error(task.getException().getMessage());
                             }
                     }
                 });
@@ -276,7 +276,7 @@ public class GooglePlus extends CordovaPlugin implements GoogleApiClient.OnConne
                             if (task.isSuccessful()){
                                 savedCallbackContext.success("Disconnected user");
                             } else {
-                                savedCallbackContext.error(task.getException());
+                                savedCallbackContext.error(task.getException().getMessage());
                             }
                     }
                 });
